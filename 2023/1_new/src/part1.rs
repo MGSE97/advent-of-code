3f91c12fb1af7acf45e3dbde1cc2b5504f2b32ec
MGSE97 <elektrikar97@gmail.com>
2023-12-05 18:57:28 +0100

    feat(2023): use template for day 1 part 1 - Restored from git history

--- 2023/1_new/src/part1.rs
+++ 2023/1_new/src/part1.rs
@@ -1,17 +1,63 @@
 use colored::Colorize;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 pub async fn solve() {
-    // Code here
-    todo!();
+    println!(
+        "Calibration document in: {file}\n",
+        file = DATA_FILE.yellow()
+    );
 
-    print!(
+    let sum = sum_calibration_data()
+        .map_err(|err| panic!("{err}"))
+        .unwrap();
+
+    println!(
         "{prefix}: {question}",
         prefix = "Question".blue(),
-        question = "".yellow().bold()
+        question = "What is the sum of all of the calibration values?"
+            .yellow()
+            .bold()
     );
-    print!(
+    println!(
         "{prefix}: {answer}",
         prefix = "  Answer".blue(),
-        answer = "".green().bold()
+        answer = sum.to_string().green().bold()
     );
+}
+
+const DATA_FILE: &str = "./data/input.txt";
+
+fn sum_calibration_data() -> Result<u32, String> {
+    // Open file reader
+    let file = File::open(DATA_FILE)
+        .map_err(|err| format!("Failed to open calibration document!\n{err}"))?;
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+    let mut sum = 0;
+    loop {
+        // Read line of calibration data
+        let read = reader
+            .read_line(&mut line)
+            .map_err(|err| format!("Failed to read calibration document!\n{err}"))?;
+
+        // Now parse them
+        let mut numbers = line.chars().filter_map(|c| char::to_digit(c, 10));
+        let first = numbers.next().unwrap_or(0);
+        let last = numbers.next_back().unwrap_or(first);
+
+        // Update sum
+        sum += first * 10 + last;
+
+        if read == 0 {
+            // Reached EOF, stop reading
+            break;
+        }
+        line.clear();
+    }
+
+    Ok(sum)
 }
